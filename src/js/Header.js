502ce26b579700918243a364debdb30ed636934d
rohyadav <rohityadav.techie@gmail.com>
2018-07-09 23:03:27 +0530

    :sparkles: init

---
+++ src/js/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Filter from './Filter';
+import image from '../images/house-location-pin.svg';
+
+const Header = ({ filterIsVisible, toggleFilter, handleFilterChange, clearFilter }) => (
+        <div>
+            <header className={`${filterIsVisible ? 'filter-is-visible' : ''}`}>
+                <Filter
+                    toggleFilter={ toggleFilter }
+                    handleFilterChange={ handleFilterChange }
+                    clearFilter={ clearFilter }
+                />
+                <img src={image} />
+                <h1>Property Sale</h1>
+                <button className="btn-filter" onClick={ e => toggleFilter(e) }>Filter</button>
+            </header>
+        </div>
+    );
+
+Header.propTypes = {
+    filterIsVisible: PropTypes.bool.isRequired,
+    toggleFilter: PropTypes.func.isRequired,
+};
+
+export default Header;
